333e6635a897fb94fabfde4aeba7f98bac9701f3
AlexZalez <remixsicool@gmail.com>
2024-01-07 13:47:36 -0400

    Ajustes de responsive

--- src/components/CV.vue
+++ src/components/CV.vue
@@ -11,7 +11,7 @@
                     <div>Alexis Jose</div> 
                     <div>Gonzalez Martinez</div>
                 </div>
-                <div class="mt-3 text-center">INGENIERO EN INFORMATICA</div>
+                <div class="mt-3 text-center lg:text-xl">INGENIERO EN INFORMATICA</div>
                 <div class="flex justify-center relative h-32">
                     <div class="border-4 border-white lg:w-52 w-36 aspect-square rounded-full overflow-hidden absolute top-[20%]">
                         <img :src="avatar" alt="" class="w-full h-full">
@@ -19,13 +19,17 @@
                 </div>
             </div>
             <div class="h-full lg:px-6 pl-3 pr-1 space-y-4">
+
+                <!-- DATOS PERSONALES -->
                 <div>
                     <div class="titulo">Datos Personales</div>
-                    <div class="flex flex-col lg:pl-3">
+                    <div class="flex flex-col lg:pl-3 md:text-base text-sm">
                         <div class="">+58 424 368 16 83</div>
-                        <div class="text-base">alexjg.181@gmail.com</div>
+                        <div class="">alexjg.181@gmail.com</div>
                     </div>
                 </div>
+
+                <!-- COMPETENCIAS -->
                 <div>
                     <div class="titulo">Competencias</div>
                     <div class="flex flex-col">
@@ -36,16 +40,27 @@
                         <div class="">Git</div>
                     </div>
                 </div>
+
+                <!-- IDIOMAS -->
                 <div>
                     <div class="titulo">Idiomas</div>
                     <div class="flex flex-col">
                         <div class="">Ingles</div>
                     </div>
                 </div>
+
+                <!-- PROYECTOS -->
                 <div >
                     <div class="titulo">Proyectos</div>
                     <div class="flex flex-col">
-                        <div class="">DolarWeb</div>
+                        <a href="https://alexzalez.github.io/DolarWeb/" class=" inline-flex items-center">
+                            <div class="text-lg tracking-wide text-blue-600 font-semibold">DolarWeb</div>
+                            <div class="w-4 aspect-square ml-0.5 stroke-blue-400 ">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="w-full" viewBox="0 0 15 15" fill="none">
+                                  <path fill-rule="evenodd" clip-rule="evenodd" d="M3 2C2.44772 2 2 2.44772 2 3V12C2 12.5523 2.44772 13 3 13H12C12.5523 13 13 12.5523 13 12V8.5C13 8.22386 12.7761 8 12.5 8C12.2239 8 12 8.22386 12 8.5V12H3V3L6.5 3C6.77614 3 7 2.77614 7 2.5C7 2.22386 6.77614 2 6.5 2H3ZM12.8536 2.14645C12.9015 2.19439 12.9377 2.24964 12.9621 2.30861C12.9861 2.36669 12.9996 2.4303 13 2.497L13 2.5V2.50049V5.5C13 5.77614 12.7761 6 12.5 6C12.2239 6 12 5.77614 12 5.5V3.70711L6.85355 8.85355C6.65829 9.04882 6.34171 9.04882 6.14645 8.85355C5.95118 8.65829 5.95118 8.34171 6.14645 8.14645L11.2929 3H9.5C9.22386 3 9 2.77614 9 2.5C9 2.22386 9.22386 2 9.5 2H12.4999H12.5C12.5678 2 12.6324 2.01349 12.6914 2.03794C12.7504 2.06234 12.8056 2.09851 12.8536 2.14645Z" fill="#000000"/>
+                                </svg>
+                            </div>
+                        </a>
                     </div>
                 </div>
 
@@ -53,6 +68,8 @@
         </aside>
         <main class="w-full pb-3 pt-1 lg:overflow-hidden overflow-y-scroll">
             <section class="">
+
+                <!-- PERFIL -->
                 <div class="px-4">
                     <div class="titulo">Perfil</div>
                     <div class="flex flex-col pl-3">
@@ -68,6 +85,7 @@
                     </div>
                 </div>
 
+                <!-- FORMACION -->
                 <div class="px-4">
                     <div class="titulo">Formación</div>
                     <div class="flex flex-col pl-3">
@@ -79,6 +97,7 @@
                     </div>
                 </div>
 
+                <!-- EXPERIENCIAS -->
                 <div class="px-4">
                     <div class="titulo">Experiencia</div>
                     <div class="flex flex-col pl-3 gap-4">
@@ -145,7 +164,7 @@
                     </div>
                 </div>
 
-                
+                <!-- REFERENCIAS -->
                 <div class="px-4 ">
                     <div class="titulo">Referencias</div>
                     <div class="flex flex-wrap space-y-2 max-h-full pl-3  pt-2">
@@ -163,6 +182,7 @@
                         </div>
                     </div>
                 </div>
+
             </section>
         </main>
     </div>
